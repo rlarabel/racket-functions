0ca8cfe33e7d99d603d5f641e09f9d4ac2b27971
ramses <rlarabel@gmail.com>
2024-03-17 19:17:49 -0400

    problems 8 and 13

--- count.rkt
+++ count.rkt
@@ -2,5 +2,16 @@
 
 (provide count-parens-all)
 
+
 (define (count-parens-all L)
-  (void))
+  (define x 0)
+  (define (count-parens L)
+    (cond
+      [(not (list? L)) '()]
+      [(and (null? L) (list? L)) (set! x (+ x 2))]
+      [(list? (first L))  (count-parens (first L)) (count-parens (rest L))]
+      [else (count-parens (rest L))]
+    ))
+  (count-parens L)
+  x
+  )
--- swapper.rkt
+++ swapper.rkt
@@ -3,4 +3,10 @@
 (provide swapper)
 
 (define (swapper a b L)
-  (void))
+  (cond
+    [(null? L) '()]
+    [(eq? a (first L)) (cons b (swapper a b (rest L)))]
+    [(eq? b (first L)) (cons a (swapper a b (rest L)))]
+    [(if (not (or (list? (first L)) (null? (first L)))) (cons (first L) (swapper a b (rest L))) (cons (swapper a b (first L)) (swapper a b (rest L))))]
+  )
+)
